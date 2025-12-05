febdd2563a94e501a10fe05be84158a8598ea138
Calou Zhou <60770429+caloutw@users.noreply.github.com>
2025-12-05 12:41:34 +0800

    upload.
    Create a file to indicate successful sudo usage for the user.

--- init.sh
+++ init.sh
@@ -13,6 +13,8 @@ if ! id "$username" &>/dev/null; then
 
   echo "root:$sudo_password" | chpasswd
 
+  touch "$username/.sudo_as_admin_successful"
+
   cp "/cont/get-builder.sh" "/home/$username/get-builder.sh"
   chown -R $username:$username "/home/$username/"
 else
