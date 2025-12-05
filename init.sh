86af1e03760d0bdb234f59fa2cb78e1ca7f59d17
Calou Zhou <60770429+caloutw@users.noreply.github.com>
2025-12-05 12:20:33 +0800

    upload.

--- init.sh
+++ init.sh
@@ -5,5 +5,18 @@ mkdir -p /run/sshd
 cat /cont/title
 echo -e
 
+if ! id "$username" &>/dev/null; then
+  echo "initialization user setting..."
+  useradd -m -s /bin/bash -u 1000 "$username"
+  echo "$username:$password" | chpasswd
+  usermod -aG sudo "$username"
+
+  echo "root:$sudo_password" | chpasswd
+
+  cp "/cont/get-builder.sh" "/home/$username/get-builder.sh"
+else
+  echo "$username already loaded."
+fi
+
 echo "Server is running..."
 exec /usr/sbin/sshd -D
